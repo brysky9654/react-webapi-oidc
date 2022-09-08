bdcff8c160bd11a797f7c4af5fb9713b0303754f
brysky9654 <brysky456@outlook.com>
2022-09-08 14:07:45 -0700

    Create a new Axios instance if token is expired

--- frontend/rwo-frontend/src/hooks/useAxios.ts
+++ frontend/rwo-frontend/src/hooks/useAxios.ts
@@ -8,19 +8,24 @@ export const useAxios = (baseURL: string) => {
     const axiosInstance = useRef<AxiosInstance>();
     const {keycloak, initialized} = useKeycloak();
     const kcToken = keycloak?.token ?? '';
+    const authenticated = keycloak.authenticated;
+    const invalidateClient =  initialized && authenticated && keycloak.isTokenExpired();
 
     useEffect(() => {
+
+        console.log("Creating a new HTTP client (axios).")
+
         axiosInstance.current = axios.create({
             baseURL,
             headers: {
-                Authorization: initialized && keycloak.authenticated ? `Bearer ${kcToken}` : false,
+                Authorization: initialized && authenticated ? `Bearer ${kcToken}` : false,
             },
         });
 
         return () => {
             axiosInstance.current = undefined;
         };
-    }, [baseURL, initialized, kcToken, keycloak.authenticated]);
+    }, [baseURL, initialized, kcToken, authenticated, invalidateClient]);
 
     return axiosInstance;
 };
